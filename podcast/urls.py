71b389524cd7a0118a52f1774a4b3197900536d8
kokopelli314 <nacclonts@gmail.com>
2017-04-28 20:27:46 -0600

    added episode list page

--- podcast/urls.py
+++ podcast/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.episode_list, name='episode_list'),
     url(r'^episode/(?P<slug>[\w\-]+)$', views.EpisodeView.as_view(), name='episode'),
+    url(r'^episodes$', views.episode_list, name='episode_list'),
 ]
