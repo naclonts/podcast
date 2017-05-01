275aacf37a708b5e547f89ecf4726e930580011c
kokopelli314 <nacclonts@gmail.com>
2017-04-30 22:33:28 -0600

    added drawing of sin wave seeker bar (not yet functional)

--- podcast/static/podcast/js/player.js
+++ podcast/static/podcast/js/player.js
@@ -1,18 +1,23 @@
-"use strict";
+'use strict';
+//
+// To do:
+// - add listener to seeker to change location
+// - update seeker position every few seconds
 
 /* global d3, document */
 var playButton = {
-    el: document.querySelector(".js-button"),
+    el: document.querySelector('.js-button'),
     audio: document.getElementById('player-audio'),
+    ctx: document.getElementById('seeker-canvas').getContext('2d'),
 
     states: {
         playing: {
-            nextState: "paused",
-            iconEl: document.querySelector("#pause-icon")
+            nextState: 'paused',
+            iconEl: document.querySelector('#pause-icon')
         },
         paused:  {
-            nextState: "playing",
-            iconEl: document.querySelector("#play-icon")
+            nextState: 'playing',
+            iconEl: document.querySelector('#play-icon')
         }
     },
 
@@ -21,28 +26,29 @@ var playButton = {
     init: function () {
         this.setInitialState();
         this.replaceUseEl();
-        this.el.addEventListener("click", this.goToNextState.bind(this));
+        this.drawSeeker();
+        this.el.addEventListener('click', this.goToNextState.bind(this));
     },
 
     setInitialState: function () {
-      var initialIconRef = this.el.querySelector("use").getAttribute("xlink:href");
-      var stateName = this.el.querySelector(initialIconRef).getAttribute("data-state");
+      var initialIconRef = this.el.querySelector('use').getAttribute('xlink:href');
+      var stateName = this.el.querySelector(initialIconRef).getAttribute('data-state');
       this.setState(stateName);
     },
 
     replaceUseEl: function () {
-        d3.select(this.el.querySelector("use")).remove();
-        d3.select(this.el.querySelector("svg")).append("path")
-            .attr("class", "js-icon")
-            .attr("d", this.stateIconPath());
+        d3.select(this.el.querySelector('use')).remove();
+        d3.select(this.el.querySelector('svg')).append('path')
+            .attr('class', 'js-icon')
+            .attr('d', this.stateIconPath());
     },
 
     goToNextState: function () {
         this.setState(this.state.nextState);
 
-        d3.select(this.el.querySelector(".js-icon")).transition()
+        d3.select(this.el.querySelector('.js-icon')).transition()
             .duration(this.animationDuration)
-            .attr("d", this.stateIconPath());
+            .attr('d', this.stateIconPath());
 
         if (this.audio.paused) {
             this.audio.play();
@@ -56,7 +62,23 @@ var playButton = {
     },
 
     stateIconPath: function () {
-        return this.state.iconEl.getAttribute("d");
+        return this.state.iconEl.getAttribute('d');
+    },
+
+    drawSeeker: function () {
+        var w = this.ctx.canvas.width;
+        var h = this.ctx.canvas.height;
+        var mid = h / 2;
+        var amp = h / 2.2;
+
+        this.ctx.moveTo(0, mid);
+        for (var x=0; x < 360; x++) {
+            var y = mid + Math.sin(x*Math.PI / 30) * amp;
+            this.ctx.lineTo(x, y);
+        }
+        this.ctx.strokeStyle = 'black';
+        this.ctx.lineWidth = 3;
+        this.ctx.stroke();
     }
 };
 
