4c6d3df445b1bf9fcf3bdb36ca46df8d6281993c
kokopelli314 <nacclonts@gmail.com>
2017-04-30 19:39:08 -0600

    cleaned up CSS

--- podcast_site/views.py
+++ podcast_site/views.py
@@ -4,13 +4,13 @@ from django.views import generic
 
 from podcast import models
 
+# Returns 5 most recent episodes for context
 def home_page(request):
     episodes = get_episode_list()
-    context = { 'episodes': episodes }
+    context = { 'episodes': episodes[0:5] }
     return render(request, 'home.html', context=context)
 
 
 def get_episode_list():
     episodes = models.Episode.objects.all()
-    print(episodes)
     return episodes
